6fa89cb4c5673c278668583d028abbe7b359b7eb
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-05-21 23:57:37 +0000

    Completed 5.2 Practice Problem

---
+++ src/main/java/Circle.java
@@ -0,0 +1,28 @@
+public class Circle {
+
+    protected double radius;
+
+    public Circle (double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double diameter() {
+        return this.radius*2.0;
+    }
+
+    public double perimeter() {
+        return (this.radius*2.0)*Math.PI;
+    }
+
+    public double area() {
+        return (Math.PI*(Math.pow(this.radius, 2.0)));
+    }
+}
